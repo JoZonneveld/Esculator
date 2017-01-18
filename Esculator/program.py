05ccecdb6bcf61c7807c4b32fe47aea71d8c332f
JoZonneveld <joostz14@gmail.com>
2017-01-18 19:33:37 +0100

    .

--- Esculator/program.py
+++ Esculator/program.py
@@ -14,15 +14,29 @@ class Elevator:
         self.Y = y
 
     def draw_elevator(self, screen):
-        pygame.draw.rect(screen, hover_red, (self.X, self.Y, 300, 25))
+        pygame.draw.rect(screen, red, (self.X, self.Y, 300, 25))
+        
+    def draw_floor(self,screen, x, y, b, h):
+        pygame.draw.rect(screen, green, (x, y, b, h))
+        
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        
+            
 
     def update(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
             self.Y -= 1
+            print("up")
         elif keys[pygame.K_DOWN]:
             self.Y += 1
+            print("down")
+        
+        
+        
 
 elevator = Elevator(300, 700)
 
@@ -41,4 +55,5 @@ def program():
         pygame.display.set_caption('Esculator')
         screen.fill(Deep_Sky_Blue)
         elevator.draw_elevator(screen)
+        elevator.draw_floor(screen, 650, 125, 50, 600)
         pygame.display.flip()
