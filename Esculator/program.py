877b2d7d4fd1c34c3bfae1d095d930098f3b744e
JoZonneveld <joostz14@gmail.com>
2017-01-18 16:47:05 +0100

    ,,..

--- Esculator/program.py
+++ Esculator/program.py
@@ -9,13 +9,22 @@ def process_events():
     return True
 
 class Elevator:
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
 
-    def draw_elevator(self, screen, x, y):
-        pygame.draw.rect(screen, hover_red, (x, y, 300, 25))
+    def draw_elevator(self, screen):
+        pygame.draw.rect(screen, hover_red, (self.X, self.Y, 300, 25))
 
-elevator = Elevator()
+    def update(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.Y -= 1
+        elif keys[pygame.K_DOWN]:
+            self.Y += 1
+
+elevator = Elevator(300, 700)
 
 def program():
     pygame.init()
@@ -28,6 +37,8 @@ def program():
     screen = pygame.display.set_mode(size)
     
     while process_events():
+        elevator.update()
+        pygame.display.set_caption('Esculator')
         screen.fill(Deep_Sky_Blue)
-        elevator.draw_elevator(screen, int(width*0.35), 700)
+        elevator.draw_elevator(screen)
         pygame.display.flip()
